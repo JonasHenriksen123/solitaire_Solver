4c8eecbfe572245268bfb26110698a4f25332eac
JonasHenriksen123 <56156281+JonasHenriksen123@users.noreply.github.com>
2021-04-14 10:33:37 +0200

    misc

--- webapp/src/main/java/group_01/webapp/ExampleController.java
+++ webapp/src/main/java/group_01/webapp/ExampleController.java
@@ -1,5 +1,7 @@
 package group_01.webapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ExampleController {
 
     @RequestMapping("/")
+    @GetMapping
     public String index()
     {
         return "This is an example request";
     }
 
+    @PostMapping
+    public void post(String postPackage)
+    {
+        //do something
+        System.out.println(postPackage);
+    }
 }
