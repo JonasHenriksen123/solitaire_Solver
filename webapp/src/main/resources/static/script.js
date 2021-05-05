f0c87d41de5d15ac469839f732956c19f91b64fa
Thorsager1 <s195470@student.dtu.dk>
2021-05-05 08:38:22 +0200

    webcam fungerer nu, dog ikke så pænt endnu

--- webapp/src/main/resources/static/script.js
+++ webapp/src/main/resources/static/script.js
@@ -1,3 +1,6 @@
+//import Webcam from 'webcam-easy';
+
+
 function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
 }
@@ -9,3 +12,19 @@ function goToGit(){
 function startGame(){
     window.location="camera.html";
 }
+
+//const webcamElement = document.getElementById("webcam");
+//const canvasElement = document.getElementById("canvas");
+//const snapSoundElement = document.getElementById('snapsound');
+//const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+/*
+webcam.start()
+    .then(result =>{
+        console.log("webcam started");
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+
+ */
