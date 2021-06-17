92258f8c8f1d2908e7e268c5621b6cb9eb9379dc
Thorsager1 <s195470@student.dtu.dk>
2021-06-17 08:30:55 +0200

    update på at sende encoded billede afsted

--- webapp/src/main/resources/static/script.js
+++ webapp/src/main/resources/static/script.js
@@ -11,9 +11,39 @@ function startGame(){
 }
 
 function userAction() {
-    fetch('http://localhost:8000/')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    //fetch('http://localhost:8000/')
+    //    .then(response => response.json())
+    //    .then(data => console.log(data));
+    var canvas = document.getElementById("canvas");
+    var video = document.getElementById("video");
+
+    canvas.getContext('2d').drawImage(video,0,0);
+    var imageBase64 = canvas.toDataURL("image/jpeg")
+    console.log(imageBase64)
+
+    fetch("http://localhost:8000",{
+        method: 'post',
+        header:{'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+    },
+        body: JSON.stringify({a: 7, str: imageBase64})
+    }).then(res => res.json())
+        .then(res => console.log(res));
+
+    /*
+    var payload = {imageBase64};
+    var data = new FormData();
+    data.append("json", JSON.stringify(payload));
+
+    fetch("http://localhost:8000/",{
+        method: "POST",
+        body: data
+    })
+        .then(function (res){return res.json()})
+        .then(function (data){alert(JSON.stringify(data))})
+        .then(data => console.log(data))
+
+     */
 }
 
 
